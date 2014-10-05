39d6f57ac30c3baf3ed41224693a61168db84d4d
Tianjiu <372615958@qq.com>
2014-10-04 22:04:55 -0700

    coding epi

--- cpp/KMP_Matching.cpp
+++ cpp/KMP_Matching.cpp
@@ -59,7 +59,7 @@ int match2(string s,string p){
 }
 
 int main(){
-	string s="ababsdfsdfccababxcadsfsdf";
+	string s="ababsdfsdfccababcadsfsdf";
 	string p="ababca";
 	cout<<(match2(s,p)==s.find(p))<<endl;
 }
