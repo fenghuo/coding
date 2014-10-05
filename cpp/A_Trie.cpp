f484736df7ea41f75a4e43901f9632a48c12cd36
Tianjiu <372615958@qq.com>
2014-10-05 15:30:36 -0700

    coding epi

--- cpp/A_Trie.cpp
+++ cpp/A_Trie.cpp
@@ -8,6 +8,38 @@ struct MAP{
 	unordered_map<char,MAP*> m;
 };
 
-int main(){
+class Trie{
+public:
+	Trie(){
+		root = new MAP();
+	}
+	void insert(const string&s){
+		auto* p = root;
+		for(const auto & c:s){
+			if(!p->m.count(c))
+				(*p)[c]=new MAP();
+			p=(*p)[c];
+		}
+	}
+	string prefix(const string&s){
+		string pre="";
+		auto* p = root;
+		for(const auto & c:s){
+			if(p->m.count(c)){
+				pre+=c;
+				p=(*p)[c];
+			} else
+				break;
+		}
+		return pre;
+	}
+private:
+	MAP*root;
+};
 
+int main(){
+	Trie trie;
+	string s1="abcdefgh";
+	trie.insert(s1);
+	cout<<trie.prefix("abc")<<endl;
 }
