ab4babc1e7d8bc96fb79390023638fa7e01f68d5
Tianjiu <372615958@qq.com>
2014-10-05 16:03:29 -0700

    coding epi

--- cpp/Merge_sorted_arrays.h
+++ cpp/Merge_sorted_arrays.h
@@ -12,7 +12,31 @@ using std::pair;
 using std::priority_queue;
 using std::vector;
 
-// @include
+struct Com{
+	bool operator()(const pair<int,int>&l, const pair<int,int>&r){
+		return l.first>r.first;
+	}
+};
+
+vector<int> MergeArrays(const vector<vector<int>>& S) {
+	priority_queue<pair<int,int>,vector<pair<int,int> >,Com> pq;
+	vector<int> idx(S.size(),1);
+	vector<int> res;
+	for(size_t i=0;i<S.size();i++)
+		pq.push({arr[0],i});
+	while(pq.size()){
+		auto&top=pq.top();
+		pq.pop();
+		res.push_back(top.first);
+		auto i = top.second;
+		idx[i]++;
+		if(idx[i]<S[i].size())
+			pq.push({S[i][idx[i]],i});
+	}
+	return res;
+}
+
+/* @include
 struct Compare {
   bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs) {
     return lhs.first > rhs.first;
@@ -45,3 +69,5 @@ vector<int> MergeArrays(const vector<vector<int>>& S) {
 }
 // @exclude
 #endif  // SOLUTIONS_MERGE_SORTED_ARRAYS_H_
+
+*/
