f86163bd9acee7c3365f6977ff85b999c31261ff
Tianjiu <372615958@qq.com>
2014-10-05 15:46:32 -0700

    coding epi

--- cpp/Counting_Sort.cpp
+++ cpp/Counting_Sort.cpp
@@ -15,8 +15,8 @@ int* countingSort(int*A,int n){
 }
 
 int main(){
-	int A[20]={4,3,2,1,4,5,6,7,6,3,2,1,2,4,5,1,2,3,4};
 	cout<<11;
+	int A[20]={4,3,2,1,4,5,6,7,6,3,2,1,2,4,5,1,2,3,4};
 	A=countingSort(A,20);
 	for(int i=0;i<20;i++)
 		cout<<A[i]<<" ";
