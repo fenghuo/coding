bd3214128a488afa950a3bdc8328745bbc2f17fc
Tianjiu <372615958@qq.com>
2014-10-07 21:44:29 -0700

    coding epi

--- cpp/FILE.cpp
+++ cpp/FILE.cpp
@@ -5,7 +5,20 @@
 #include<unordered_map>
 #include<functional>
 
+
 using namespace std;
+
+struct String{
+	string s;
+	String(string s):s(s){}
+	bool operator==(const String&right) const {
+		return s==right.s;
+	}	
+	ostream&operator<<(ostream&os, const String s){
+		os<<s;
+		return os;
+	}
+}
 
 struct Hash{
 	size_t operator()(const string&s) const{
@@ -19,7 +32,7 @@ int main(){
 	int p=0;
 	unordered_map<string,int,Hash> dict;
 	while(getline(in,s))
-		dict[s]=++p;
+		dict[String(s)]=++p;
 	for(auto entry:dict)
 		cout<<entry.first<<" - "<<entry.second<<" : "<<dict.hash_function()(entry.first)<<endl;
 	return 0;
