911f2acaa11e84e92114d67e4f2d561a2bec0e99
Tianjiu <372615958@qq.com>
2014-10-07 21:39:47 -0700

    coding epi

--- cpp/FILE.cpp
+++ cpp/FILE.cpp
@@ -7,11 +7,17 @@
 
 using namespace std;
 
+struct Hash{
+	size_t operator()(const string&s) const{
+		return 1;
+	}
+};
+
 int main(){
 	ifstream in("t");
 	string s;
 	int p=0;
-	unordered_map<string,int> dict;
+	unordered_map<string,int,Hash> dict;
 	while(getline(in,s))
 		dict[s]=++p;
 	for(auto entry:dict)
