fcb8754f364bbd53294b7b7eb6290fc568212b98
Tianjiu <372615958@qq.com>
2014-10-08 23:52:15 -0700

    coding epi

---
+++ cpp/Changes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void change(int sum, vector<int>&changes, vector<int>&path){
+	if(sum==0){
+		for(auto i:path)
+			cout<<i<<" ";
+		cout<<endl;
+	} else if(sum>0){
+		for(auto i:changes){
+			path.push_back(i);
+			change(sum-i,changes,path);
+			path.pop_back();
+		}
+	}
+} 
+
+int main(){
+	vector<int> changes={2,3,7};
+	vector<int> path;
+	change(20,changes,path);
+}
