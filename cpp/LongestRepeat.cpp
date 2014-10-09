3835a64aac9ed22179cb700076f6837597d21342
Tianjiu <372615958@qq.com>
2014-10-09 13:44:18 -0700

    coding epi

---
+++ cpp/LongestRepeat.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+
+using namespace std;
+
+Trie{
+	unordered_map<char, Trie*> m;
+};
+
+string find(string&S){
+	Trie root;
+	int max=0;
+	int start=0;
+	vector<Trie*> pointers={&root};
+	for(size_t i=0;i<S.size();i++){
+		auto*curr=&root;
+		for(int j=i;j<S.size();j++){
+			if(curr->m.count(S[j])){
+				if(j-i+1>max){
+					max=j-i+1;
+					start=i;
+				}
+			} else{
+				curr->m[S[j]]=new Trie();
+				pointers.push_back(curr->m[S[j]]);
+			}
+			curr=curr->m[S[j]];
+		}
+	}
+	for(auto p:pointers)
+		delete p;
+	return S.substr(start,max);
+}
+
+int main(){
+	string s;
+	getline(cin,s);
+	cout<<find(s)<<endl;
+}
