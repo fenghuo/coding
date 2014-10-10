cc7cbb6568752c5865f5c3d9af50dc25dc1098f5
Tianjiu <372615958@qq.com>
2014-10-10 00:25:53 -0700

    coding epi

--- cpp/Auto.cpp
+++ cpp/Auto.cpp
@@ -7,7 +7,7 @@ int main(){
 	vector<int*> s;
 	for(int i=0;i<10;i++){
 		s.push_back(&i);
-		cout<<s[0]<<" "<<s.capacity()<<endl;
+		cout<<&s[0]<<" "<<s.capacity()<<endl;
 	}
 	for(auto t:s)
 		cout<<t<<",";
