b0c109ccd16bd7fc08e641395031803115ad44ce
Tianjiu <372615958@qq.com>
2014-10-09 23:47:44 -0700

    coding epi

--- cpp/Drawing_skylines.cpp
+++ cpp/Drawing_skylines.cpp
@@ -59,6 +59,7 @@ vector<Skyline> drawing_skylines(vector<Skyline> skylines) {
 		points.push_back(Endpoint(line.left,line.height,true));
 		points.push_back(Endpoint(line.right,line.height,false));
 		points[points.size()-1].left=&(points[points.size()-2]);
+		cout<<points.size()<<endl;
 	}
 	points[1].left->valid=false;
 	cout<<points[0].valid<<" --- "<<endl;
