95d5250b0fae3b65c3a91ddd900c39eeac3e5a10
Tianjiu <372615958@qq.com>
2014-10-14 18:57:45 -0700

    coding epi

--- cpp/Sramble.cpp
+++ cpp/Sramble.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<string>
 #include<unordered_map> 
+#include<algorithm>
 
 using namespace std;
 
 unordered_map<string,bool> dict;
 
-	bool check(string&s1,string&s2){
+	bool check(string s1,string s2){
+		sort(s1.begin(),s1.end());
+		sort(s2.begin(),s2.end());
+		return s1==s2;
 		int count[256]={0};
 		for(auto c:s1)
 			count[c]++;
