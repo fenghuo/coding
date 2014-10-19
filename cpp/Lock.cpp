0ddcff99ff6c3409d4898a8e4b65cbc34163306b
Tianjiu <372615958@qq.com>
2014-10-18 20:48:57 -0700

    coding epi

--- cpp/Lock.cpp
+++ cpp/Lock.cpp
@@ -10,12 +10,12 @@ mutex mtx;
 int i=0;
 
 void print_thread_id (int id) {
-     //mtx.lock();
+     mtx.lock();
 	int k=i;
 	for(int z=0;z<1000000;z++)
 		i=k+1;
      std::cout << "thread #" << i << '\n';
-	//mtx.unlock();
+	mtx.unlock();
 }
   
         int main ()
