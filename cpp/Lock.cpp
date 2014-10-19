403d72399753feebdfb28b77d6bd5105308dd6dc
Tianjiu <372615958@qq.com>
2014-10-18 20:47:46 -0700

    coding epi

--- cpp/Lock.cpp
+++ cpp/Lock.cpp
@@ -12,7 +12,7 @@ int i=0;
 void print_thread_id (int id) {
      //mtx.lock();
 	int k=i;
-	for(int z=0;z<1000000;z++);
+	for(int z=0;z<1000000;z++)
 		i=k+1;
      std::cout << "thread #" << i << '\n';
 	//mtx.unlock();
@@ -20,9 +20,9 @@ void print_thread_id (int id) {
   
         int main ()
         {
-              std::thread threads[10];
+              std::thread threads00[1i000];
               // spawn 10 threads:
-              for (int i=0; i<10; ++i)
+              for (int i=0; i<1000; ++i)
                   threads[i] = std::thread(print_thread_id,i+1);
               for (auto& th : threads) th.join();
 	
